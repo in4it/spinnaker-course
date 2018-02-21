a49f5aecb4c303c7c424d45ad504e2297e7e835f
Edward Viaene <ward.viaene@gmail.com>
2018-02-21 15:59:40 +0000

    spinnaker scripts

---
+++ scripts/create-user.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+groupadd ubuntu
+useradd -g ubuntu -G admin -s /bin/bash -d /home/ubuntu ubuntu
+mkdir -p /home/ubuntu
+cp -r /root/.ssh /home/ubuntu/.ssh
+chown -R ubuntu:ubuntu /home/ubuntu
