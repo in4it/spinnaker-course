15b07b26e7b811a913d21f785ef99644b2af19b3
Jorn Jambers <jorn@in4it.io>
2018-03-02 10:35:54 +0100

    add ubuntu to sudoers

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -4,3 +4,4 @@ useradd -g ubuntu -G admin -s /bin/bash -d /home/ubuntu ubuntu
 mkdir -p /home/ubuntu
 cp -r /root/.ssh /home/ubuntu/.ssh
 chown -R ubuntu:ubuntu /home/ubuntu
+echo "ubuntu	ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
