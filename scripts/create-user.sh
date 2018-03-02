1941e612a8a11871c97d83b7486442e850effeb8
Jorn Jambers <jorn@in4it.io>
2018-03-02 11:42:09 +0100

    sequence

--- scripts/create-user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-groupadd ubuntu
-useradd -g ubuntu -G admin -s /bin/bash -d /home/ubuntu ubuntu
-mkdir -p /home/ubuntu
-cp -r /root/.ssh /home/ubuntu/.ssh
-chown -R ubuntu:ubuntu /home/ubuntu
-echo "ubuntu	ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
