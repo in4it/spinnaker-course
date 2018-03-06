a62e41e98003ab62e8d65656450d6ddeb9ce41f6
Jorn Jambers <jorn@in4it.io>
2018-03-06 10:32:30 +0100

    Add Halyard user ubuntu

--- scripts/3-install-halyard.sh
+++ scripts/3-install-halyard.sh
@@ -3,7 +3,7 @@
 set -e
 
 curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
-sudo bash InstallHalyard.sh
+sudo bash InstallHalyard.sh --user ubuntu
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker ubuntu
