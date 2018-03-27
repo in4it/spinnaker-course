e0231dbcec4b24075d125ac23966685884d932cf
Jorn Jambers <jorn@in4it.io>
2018-03-27 22:33:21 +0200

    add jenkins install

---
+++ jenkins/2-jenkins-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install openjdk-8-jdk
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins git
+sudo service jenkins start
