0b843971ffb897d26c930e1a9f4c1d91914c1682
Jorn Jambers <jorn@in4it.io>
2018-03-29 21:13:18 +0200

    docs + debian prereq

--- jenkins/3-create-debian.sh
+++ jenkins/3-create-debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update 
-sudo apt-get -y install build-essential devscripts git-buildpackage aptly gnupg rng-tools npm
+sudo apt-get -y install build-essential devscripts git-buildpackage aptly gnupg rng-tools npm docker
