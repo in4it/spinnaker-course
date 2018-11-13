ea17d0e8ae34fba8eab916e11f1a087db757b2c0
Carlos Roman <caroman@endava.com>
2018-11-13 16:53:05 -0500

    Fix to low memory devices

--- vagrant/install-spinnaker.sh
+++ vagrant/install-spinnaker.sh
@@ -7,7 +7,7 @@ export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
-sudo fallocate -l 16G /swapfile
+sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
